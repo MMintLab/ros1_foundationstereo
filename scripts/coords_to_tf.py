c0584b09f7946d6e74fa4c25959f1ddaa2b79c25
Mark Van der Merwe <markvdm@umich.edu>
2024-05-17 15:32:42 -0400

    Script for getting photoneo tf

--- scripts/coords_to_tf.py
+++ scripts/coords_to_tf.py
@@ -1,15 +1,19 @@
+import argparse
+
 import numpy as np
 import tf.transformations as tf
 
-# fn = "/home/markvdm/Documents/RotatedCalibration1.9.1/build/motioncam_4_8_23_coordinates.txt"
-fn = "/home/markvdm/Documents/RotatedCalibration1.9.1/build/photoneo_4_8_23_coordinates.txt"
-T = np.loadtxt(fn)
-# print(T)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Converts a 4x4 matrix to a TF pose")
+    parser.add_argument("filename", help="The filename of the 4x4 matrix")
+    args = parser.parse_args()
 
-x = T[0, 3] / 1000.0
-y = T[1, 3] / 1000.0
-z = T[2, 3] / 1000.0
+    T = np.loadtxt(args.filename)
 
-quat = tf.quaternion_from_matrix(T)
+    x = T[0, 3] / 1000.0
+    y = T[1, 3] / 1000.0
+    z = T[2, 3] / 1000.0
 
-print("%f %f %f %f %f %f %f" % (x, y, z, quat[0], quat[1], quat[2], quat[3]))
+    quat = tf.quaternion_from_matrix(T)
+
+    print("%f %f %f %f %f %f %f" % (x, y, z, quat[0], quat[1], quat[2], quat[3]))
