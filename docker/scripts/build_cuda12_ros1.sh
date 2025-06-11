d57ef2121381264ac833635c22f6b859328cea78
yswi <yswi@umich.edu>
2025-06-11 00:25:04 -0400

    add rospkg for build

--- docker/scripts/build_cuda12_ros1.sh
+++ docker/scripts/build_cuda12_ros1.sh
@@ -107,7 +107,8 @@ RUN cd catkin_ws/src && \
     python -m pip install flash-attn && \
     python -m pip install -e . && \
     cd .. && \
-    python -m pip install -e . "
+    python -m pip install -e . && \
+    pip install rospkg"
 
     
 # Build catkin workspace.
